d92efabc2c7cd70a08079c96e6db85da05d79c0c
Alona Shelikhova <ashelyaa17@gmail.com>
2024-09-18 14:55:26 +0300

    make build

---
+++ index.163db92a.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=t.clientHeight,c=t.clientWidth,i=e.clientHeight,o=e.clientWidth;e.style.position="absolute",e.style.top="".concat((l-i)/2,"px"),e.style.left="".concat((c-o)/2,"px");
+//# sourceMappingURL=index.163db92a.js.map
